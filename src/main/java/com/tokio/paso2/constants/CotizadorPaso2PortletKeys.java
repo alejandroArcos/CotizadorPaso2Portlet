bad7e0afb00cdfb73890da507f37c8a3c2c78a80
alejandroArcos <alejandro.arcos@globalquark.com.mx>
2023-02-23 11:29:15 -0600

    First Commit
    First Commit

---
+++ src/main/java/com/tokio/paso2/constants/CotizadorPaso2PortletKeys.java
@@ -0,0 +1,34 @@
+package com.tokio.paso2.constants;
+
+/**
+ * @author jonathanfviverosmoreno
+ */
+public class CotizadorPaso2PortletKeys {
+
+	public static final String CotizadorPaso2 = "CotizadorPaso2";
+	public static final String PORTLET_NAME = "CotizadorPaso2";
+	
+	public static final String TITULO_FAMILIAR = "Cotizador Paquete Familiar Version 1";
+	public static final String TITULO_EMPRESARIAL = "Cotizador Paquete Empresarial";
+	
+	public static final String PANTALLA_FAMILIAR = "paqueteFamiliar";
+	public static final String PANTALLA_EMPRESARIAL = "moduloEmpresarial";
+	
+	
+	public static final int PERFIL_EJECUTIVO = 1;
+	public static final int PERFIL_AGENTE = 2;
+	public static final int PERFIL_ASISTENTE = 3;
+	public static final int PERFIL_SUSCRIPTORJR = 4;
+	public static final int PERFIL_SUSCRIPTORSR = 5;
+	public static final int PERFIL_SUSCRIPTORMR = 6;
+
+	
+	public static final String SUBGIRO_RIESGO = "XS";
+	public static final String EXEDE_LIMITES = "XEL";
+	public static final String COTIZADO_SUSCRIPTOR = "CPS";
+	public static final String REVIRE_SUSCRIPTOR = "RAS";
+	public static final String TURNADO_A_492 = "T492";
+	public static final String VBA_492 = "VB492";
+	public static final String RECHAZO_VBA_492 = "RVB492";
+
+}
